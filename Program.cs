4a614f8e40d69d727b3f3dd60452f104602ef6e1
Pothigai <pothigaiselvan@gmail.com>
2024-02-07 18:19:04 +0100

    Fixes #2, replaced point values by constants that can be changed at the beginning of the code and grouped variables and constants

--- Program.cs
+++ Program.cs
@@ -5,12 +5,19 @@
         static void Main(string[] args)
         {
             bool playAgain = true;
-            int totalpoints = 0;
+            
+            int totalPoints = 0;
+            
+            const int horPoint = 20;
+            const int diagPoint = 30;
+            const int jackpot = 100;
 
             while (playAgain)
             {
                 Random rnd = new Random();
+                
                 int[,] slots = new int[3, 3];
+                
                 for (int i = 0; i < 3; i++)
                 {
                     for (int j = 0; j < 3; j++)
@@ -18,9 +25,11 @@
                         slots[i, j] = rnd.Next(0, 10);
                     }
                 }
+                
                 for (int i = 0; i < 3; i++)
                 {
                     Console.WriteLine("");
+                    
                     for (int j = 0; j < 3; j++)
                     {
                         Console.Write("   " + slots[i, j]);
@@ -29,40 +38,40 @@
 
                 if (slots[0, 0] == slots[0, 1] && slots[0, 1] == slots[0, 2])
                 {
-                    Console.WriteLine("You won 20 points!");
-                    totalpoints = totalpoints + 20;
+                    Console.WriteLine($"You won {horPoint} points!");
+                    totalPoints = totalPoints + horPoint;
                 }
                 if (slots[1, 0] == slots[1, 1] && slots[1, 1] == slots[1, 2])
                 {
-                    Console.WriteLine("You won 20 points!");
-                    totalpoints = totalpoints + 20;
+                    Console.WriteLine($"You won {horPoint} points!");
+                    totalPoints = totalPoints + horPoint;
                 }
                 if (slots[2, 0] == slots[2, 1] && slots[2, 1] == slots[2, 2])
                 {
-                    Console.WriteLine("You won 20 points!");
-                    totalpoints = totalpoints + 20;
+                    Console.WriteLine($"You won {horPoint} points!");
+                    totalPoints = totalPoints + horPoint;
                 }
                 if (slots[0, 0] == slots[1, 1] && slots[1, 1] == slots[2, 2])
                 {
-                    Console.WriteLine("You won 30 points!");
-                    totalpoints = totalpoints + 30;
+                    Console.WriteLine($"You won {diagPoint} points!");
+                    totalPoints = totalPoints + diagPoint;
                 }
                 if (slots[2, 0] == slots[1, 1] && slots[1, 1] == slots[0, 2])
                 {
-                    Console.WriteLine("You won 30 points!");
-                    totalpoints = totalpoints + 30;
+                    Console.WriteLine($"You won {diagPoint} points!");
+                    totalPoints = totalPoints + diagPoint;
                 }
                 if (slots[0, 0] == slots[0, 1] && slots[0, 1] == slots[0, 2] && slots[0, 2] == slots[1, 0] && slots[1, 0] == slots[1, 1] && slots[1, 1] == slots[1, 2] && slots[1, 2] == slots[2, 0] && slots[2, 0] == slots[2, 1] && slots[2, 1] == slots[2, 2])
                 {
-                    Console.WriteLine("You won the jackpot!");
-                    totalpoints = totalpoints + 100;
+                    Console.WriteLine($"You won the jackpot of a {jackpot} points!");
+                    totalPoints = totalPoints + jackpot;
                 }
                 else
                 {
                     Console.WriteLine("\nYou lost!");
                 }
 
-                Console.WriteLine($"Your total points are {totalpoints}");
+                Console.WriteLine($"Your total points are {totalPoints}");
                 Console.WriteLine("Do you want to play again? (y/n)");
                 string input = Console.ReadLine();
 
