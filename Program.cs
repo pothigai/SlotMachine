d4db5b5e983e135a2e5afcadd5ae813ce8ed8d8e
Pothigai <pothigaiselvan@gmail.com>
2024-02-10 15:22:53 +0100

    Modified the code to add a by in amount, check if the user would like to top up and continue, and added a cost for each round.

--- Program.cs
+++ Program.cs
@@ -4,20 +4,58 @@
     {
         static void Main(string[] args)
         {
-            bool playAgain = true;
-            
-            int totalPoints = 0;
-            
+            // Initialize points for each category
             const int horPoint = 20;
             const int diagPoint = 30;
             const int jackpot = 100;
 
-            while (playAgain)
+            //Ask the user for a min buy in of 500 points
+            Console.WriteLine("Please enter your buy in amount, the minimum is 500 points: ");
+            int totalPoints = Convert.ToInt32(Console.ReadLine());
+
+            while (totalPoints < 500)
             {
+                Console.WriteLine("The entered amount does not meet the minimum buy in amount, please enter an amount of aleast 500:");
+                totalPoints = Convert.ToInt32(Console.ReadLine());
+            }
+
+            string playAgain = "y";
+
+            while (playAgain == "y")
+            {
+                Console.Clear();
+
+
+                //Checking if user has enough points to play
+                if (totalPoints <= 0)
+                {
+                    Console.WriteLine("You do not have enough points, would you like to top up? (y/n): ");
+                    string topUp = Console.ReadLine();
+
+                    while (topUp.ToLower() != "y" && topUp.ToLower() != "n")
+                    {
+                        Console.Write("Invalid input, please enter y/n:");
+                        topUp = Console.ReadLine();
+                    }
+
+                    if (topUp.ToLower() == "y")
+                    {
+                        Console.WriteLine("Please enter the amount of points you want to purchase");
+                        totalPoints += Convert.ToInt32(Console.ReadLine());
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                totalPoints -= 50;
+                
+                //Generate the random 3x3 matrix for the slot machine
                 Random rnd = new Random();
-                
+
                 int[,] slots = new int[3, 3];
-                
+
                 for (int i = 0; i < 3; i++)
                 {
                     for (int j = 0; j < 3; j++)
@@ -25,17 +63,18 @@
                         slots[i, j] = rnd.Next(0, 10);
                     }
                 }
-                
+
                 for (int i = 0; i < 3; i++)
                 {
                     Console.WriteLine("");
-                    
+
                     for (int j = 0; j < 3; j++)
                     {
                         Console.Write("   " + slots[i, j]);
                     }
                 }
 
+                //Check win conditions for rows and diagonals
                 if (slots[0, 0] == slots[0, 1] && slots[0, 1] == slots[0, 2])
                 {
                     Console.WriteLine($"You won {horPoint} points!");
@@ -71,13 +110,19 @@
                     Console.WriteLine("\nYou lost!");
                 }
 
+                //Display round winnings and ask user if they wish to play again
                 Console.WriteLine($"Your total points are {totalPoints}");
                 Console.WriteLine("Do you want to play again? (y/n)");
-                string input = Console.ReadLine();
+                playAgain = Console.ReadLine();
 
-                if (input.ToLower() != "y")
+
                 {
-                    playAgain = false;
+                    while (playAgain.ToLower() != "y" && playAgain.ToLower() != "n")
+                    {
+                        Console.WriteLine("Invalid input. Please enter 'y' or 'n'.");
+                        playAgain = Console.ReadLine();
+                    }
+
                 }
             }
         }
